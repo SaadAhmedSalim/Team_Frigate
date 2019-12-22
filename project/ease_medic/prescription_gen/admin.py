0f4fd8f744c84fd486eb8a70cd9427f168bbed83
SaadAhmedSalim <shuvrohossain8@gmail.com>
2019-12-22 07:26:08 +0600

    Delete unnecessary files

--- project/ease_medic/prescription_gen/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from.models import Medicine, PrescriptionMedicine, Prescription
-
-admin.site.register(Medicine)
-admin.site.register(Prescription)
-admin.site.register(PrescriptionMedicine)
